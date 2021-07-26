c04aec88bfe1032298908f0c1f2164ccb89cb181
reeeckeee <munozpedro2002@gmail.com>
2021-07-26 19:56:17 -0300

    proyectoEnHeroku

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/login.html"))
 });
 
-app.listen(3030, () =>
+app.listen(process.env.PORT || 3030, () =>
     console.log("Corriendo servidor")
 );
